0265ee4751c418463bf07320194e1d6230274554
Jess Latimer <jplatimer@gmail.com>
2015-01-25 12:21:57 -0800

    fixes

--- src/android/AppListPlugin.java
+++ src/android/AppListPlugin.java
@@ -1,6 +1,6 @@
 package com.phonegap.plugins.applist;
 
-import android.util.Log;
+// import android.util.Log;
 import java.util.List;
 import org.apache.cordova.CallbackContext;
 import org.apache.cordova.CordovaPlugin;
@@ -16,52 +16,52 @@ public class AppListPlugin extends CordovaPlugin {
     @Override
     public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
 
-        callbackContext.success("1");        
+        // callbackContext.success("1");        
         try {
-        callbackContext.success("2");   
+            // callbackContext.success("2");   
             if (ACTION_GET_APP_LIST.equals(action)) { 
-        callbackContext.success("3");   
+                // callbackContext.success("3");   
                 PackageManager packageManager = getPackageManager();
-        callbackContext.success("4");   
+                // callbackContext.success("4");   
                 List<ApplicationInfo> applist = packageManager.getInstalledApplications(PackageManager.GET_META_DATA);
 
-        callbackContext.success("5");   
+                // callbackContext.success("5");   
                 JSONObject responseDetailsJson = new JSONObject();
-        callbackContext.success("6");   
+                // callbackContext.success("6");   
                 JSONArray jsonArray = new JSONArray();
-        callbackContext.success("7");   
+                // callbackContext.success("7");   
 
                 for (ApplicationInfo appInfo : applist) {
-        callbackContext.success("8");   
+                    // callbackContext.success("8");   
                     if ((appInfo.flags & ApplicationInfo.FLAG_SYSTEM) != 1) {
-        callbackContext.success("9");   
+                        // callbackContext.success("9");   
                         // Installed by user
-                        Log.w(WARN,appInfo.packageName);
-        callbackContext.success("10");   
+                        // Log.w(WARN,appInfo.packageName);
+                        // callbackContext.success("10");   
                         JSONObject appDetailsJson = new JSONObject();
-        callbackContext.success("11");   
+                        // callbackContext.success("11");   
                         appDetailsJson.put("processName", appInfo.processName);
-        callbackContext.success("12");   
+                        // callbackContext.success("12");   
                         appDetailsJson.put("packageName", appInfo.packageName);
-        callbackContext.success("13");   
+                        // callbackContext.success("13");   
 
-                        jsonArray.add(appDetailsJson);
-        callbackContext.success("14");
+                        jsonArray.put(appDetailsJson);
+                        // callbackContext.success("14");
                     } else {
                         // System application
                     }
                 }
                 responseDetailsJson.put("apps", jsonArray);
-        callbackContext.success("15");
+                // callbackContext.success("15");
                 callbackContext.success(responseDetailsJson);
-        callbackContext.success("16");
+                // callbackContext.success("16");
                 return true;
             }
             callbackContext.error("Invalid action");
-        callbackContext.success("17");
+            // callbackContext.success("17");
             return false;
         } catch(Exception e) {
-            Log.w(WARN,"Exception: " + e.getMessage());
+            // Log.w(WARN,"Exception: " + e.getMessage());
             callbackContext.error(e.getMessage());
             return false;
         } 
