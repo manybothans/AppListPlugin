4682a44fcb99822ffabcb7090c0d76bab3f7da35
Jess Latimer <jplatimer@gmail.com>
2015-01-25 14:36:14 -0800

    added ios

---
+++ src/ios/AAAppListPlugin.m
@@ -0,0 +1,258 @@
+//
+//  AAAppListPlugin.m
+//  AAAppListPlugin
+//
+//  Created by Daniel Amitay on 10/21/12.
+//  Copyright (c) 2012 Daniel Amitay. All rights reserved.
+//
+
+#import "AAAppListPlugin.h"
+
+@implementation AAAppListPlugin
+
+@synthesize country = _country;
+
+#pragma mark - Public methods
+
+- (void)getAppList:(CDVInvokedUrlCommand*)command
+{
+
+    NSString* callbackId = [command callbackId];
+    NSString* name = [[command arguments] objectAtIndex:0];
+    NSString* msg = [NSString stringWithFormat: @"Hello, %@", name];
+
+    CDVPluginResult* result = [CDVPluginResult
+                               resultWithStatus:CDVCommandStatus_OK
+                               messageAsString:msg];
+
+
+    [self detectAppIdsWithIncremental:^(NSArray *appDictionaries) {
+        NSLog(@"Incremental appDictionaries.count: %i", appDictionaries.count);
+        NSMutableArray *newAppDictionaries = [NSMutableArray arrayWithArray:self.detectedApps];
+        [newAppDictionaries addObjectsFromArray:appDictionaries];
+        self.detectedApps = newAppDictionaries;
+    } withSuccess:^(NSArray *appDictionaries) {
+        NSLog(@"Successful appDictionaries.count: %i", appDictionaries.count);
+        NSData * JSONData = [NSJSONSerialization dataWithJSONObject:appDictionaries
+                                                    options:0
+                                                      error:nil];
+        [[UIApplication sharedApplication] setNetworkActivityIndicatorVisible:NO];
+        [self success:JSONData callbackId:callbackId];
+    } withFailure:^(NSError *error) {
+        NSLog(@"Error: %@", error.localizedDescription);
+        [[UIApplication sharedApplication] setNetworkActivityIndicatorVisible:NO];
+        [self error:error.localizedDescription callbackId:callbackId];
+    }];
+
+    [[UIApplication sharedApplication] setNetworkActivityIndicatorVisible:NO];
+}
+
+- (void)detectAppIdsWithIncremental:(void (^)(NSArray *appIds))incrementalBlock
+                        withSuccess:(void (^)(NSArray *appIds))successBlock
+                        withFailure:(void (^)(NSError *error))failureBlock {
+    dispatch_queue_t detection_thread = dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0);
+    dispatch_async(detection_thread, ^{
+        [self retrieveSchemeAppsDictionaryWithSuccess:^(NSDictionary *schemeAppsDictionary) {
+            NSMutableArray *schemeDictionaries = [[NSMutableArray alloc] init];
+            for (NSString *scheme in schemeAppsDictionary.allKeys) {
+                NSArray *appIds = [schemeAppsDictionary objectForKey:scheme];
+                NSDictionary *schemeDictionary = @{@"scheme" : scheme, @"ids" : appIds};
+                [schemeDictionaries addObject:schemeDictionary];
+            }
+            UIApplication *application = [UIApplication sharedApplication];
+            NSMutableDictionary *successfulAppIds = [[NSMutableDictionary alloc] init];
+            NSArray *arrayOfArrays =  [self subarraysOfArray:schemeDictionaries withCount:1000];
+            for (NSArray *schemeDictionariesArray in arrayOfArrays) {
+                NSMutableDictionary *incrementalAppIds = [[NSMutableDictionary  alloc] init];
+                for (NSDictionary *schemeDictionary in schemeDictionariesArray) {
+                    NSString *scheme = [schemeDictionary objectForKey:@"scheme"];
+                    NSURL *url = [NSURL URLWithString:[NSString stringWithFormat:@"%@://", scheme]];
+                    if ([application canOpenURL:url]) {
+                        NSArray *appIds = [schemeDictionary objectForKey:@"ids"];
+                        for (NSString *appId in appIds) {
+                            if (successfulAppIds[appId] == nil) {
+                                successfulAppIds[appId] = [NSObject new];
+                                incrementalAppIds[appId] = [NSObject new];
+                            }
+                        }
+                    }
+                }
+                if (incrementalBlock && incrementalAppIds.count) {
+                    dispatch_async(dispatch_get_main_queue(), ^{
+                        incrementalBlock(incrementalAppIds.allKeys);
+                    });
+                }
+            }
+            dispatch_async(dispatch_get_main_queue(), ^{
+                successBlock(successfulAppIds.allKeys);
+            });
+        } failure:failureBlock];
+    });
+}
+
+- (void)detectAppDictionariesWithIncremental:(void (^)(NSArray *appDictionaries))incrementalBlock
+                                 withSuccess:(void (^)(NSArray *appDictionaries))successBlock
+                                 withFailure:(void (^)(NSError *error))failureBlock {
+    __block BOOL successBlockExecuted = FALSE;
+    __block BOOL appIdDetectionComplete = FALSE;
+    __block NSInteger netAppIncrements = 0;
+    NSMutableArray *successfulAppDictionaries = [[NSMutableArray alloc] init];
+    [self detectAppIdsWithIncremental:^(NSArray *appIds) {
+        netAppIncrements++;
+        [self retrieveAppDictionariesForAppIds:appIds
+                                   withSuccess:^(NSArray *appDictionaries) {
+                                       [successfulAppDictionaries addObjectsFromArray:appDictionaries];
+                                       incrementalBlock(appDictionaries);
+                                       netAppIncrements--;
+                                       // Unhappy with this implementation
+                                       dispatch_time_t delay = dispatch_time(DISPATCH_TIME_NOW, 0.1f * NSEC_PER_SEC);
+                                       dispatch_after(delay, dispatch_get_main_queue(), ^{
+                                           if (appIdDetectionComplete &&
+                                               !netAppIncrements &&
+                                               successBlock &&
+                                               !successBlockExecuted) {
+                                               successBlockExecuted = TRUE;
+                                               successBlock(successfulAppDictionaries);
+                                           }
+                                       });
+                                   } withFailure:^(NSError *error) {
+                                       netAppIncrements--;
+                                       // Unhappy with this implementation
+                                       dispatch_time_t delay = dispatch_time(DISPATCH_TIME_NOW, 0.1f * NSEC_PER_SEC);
+                                       dispatch_after(delay, dispatch_get_main_queue(), ^{
+                                           if (appIdDetectionComplete &&
+                                               !netAppIncrements &&
+                                               successBlock &&
+                                               !successBlockExecuted) {
+                                               successBlockExecuted = TRUE;
+                                               successBlock(successfulAppDictionaries);
+                                           }
+                                       });
+                                   }];
+    } withSuccess:^(NSArray *appIds) {
+        appIdDetectionComplete = TRUE;
+    } withFailure:failureBlock];
+}
+
+- (void)retrieveAppDictionariesForAppIds:(NSArray *)appIds
+                             withSuccess:(void (^)(NSArray *appDictionaries))successBlock
+                             withFailure:(void (^)(NSError *error))failureBlock {
+    dispatch_queue_t retrieval_thread = dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0);
+    dispatch_async(retrieval_thread, ^{
+        NSString *appString = [appIds componentsJoinedByString:@","];
+        NSMutableString *requestUrlString = [[NSMutableString alloc] init];
+        [requestUrlString appendFormat:@"http://itunes.apple.com/lookup"];
+        [requestUrlString appendFormat:@"?id=%@", appString];
+        if (self.country) {
+            [requestUrlString appendFormat:@"&country=%@", self.country];
+        }
+        
+        NSURLResponse *response = nil;
+        NSError *connectionError = nil;
+        NSMutableURLRequest *request = [[NSMutableURLRequest alloc] init];
+        [request setURL:[NSURL URLWithString:requestUrlString]];
+        [request setTimeoutInterval:20.0f];
+        [request setCachePolicy:NSURLRequestReturnCacheDataElseLoad];
+        NSData *result = [NSURLConnection sendSynchronousRequest:request
+                                               returningResponse:&response
+                                                           error:&connectionError];
+        if (connectionError && failureBlock) {
+            dispatch_async(dispatch_get_main_queue(), ^{
+                failureBlock(connectionError);
+            });
+        } else {
+            NSError *jsonError = nil;
+            NSDictionary *jsonDictionary = [NSJSONSerialization JSONObjectWithData:result
+                                                                           options:NSJSONReadingMutableLeaves
+                                                                             error:&jsonError];
+            if (jsonError && failureBlock) {
+                dispatch_async(dispatch_get_main_queue(), ^{
+                    failureBlock(jsonError);
+                });
+            } else if (successBlock) {
+                NSArray *results = [jsonDictionary objectForKey:@"results"];
+                dispatch_async(dispatch_get_main_queue(), ^{
+                    successBlock(results);
+                });
+            }
+        }
+    });
+}
+
+
+#pragma mark - Internal methods
+
+- (void)retrieveSchemeAppsDictionaryWithSuccess:(void (^)(NSDictionary *schemeAppsDictionary))successBlock
+                                        failure:(void (^)(NSError *error))failureBlock {
+    [self retrieveSchemeAppsDictionaryFromLocalWithSuccess:successBlock failure:failureBlock];
+}
+
+- (void)retrieveSchemeAppsDictionaryFromLocalWithSuccess:(void (^)(NSDictionary *schemeAppsDictionary))successBlock
+                                                 failure:(void (^)(NSError *error))failureBlock {
+    dispatch_queue_t retrieval_thread = dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0);
+    dispatch_async(retrieval_thread, ^{
+        NSBundle *selfBundle = [NSBundle bundleForClass:[self class]];
+        NSString *appSchemesDictionaryPath = [selfBundle pathForResource:@"schemeApps" ofType:@"json"];
+        if (!appSchemesDictionaryPath && failureBlock) {
+            dispatch_async(dispatch_get_main_queue(), ^{
+                failureBlock(nil);
+            });
+        } else {
+            NSError *dataError = nil;
+            NSData *schemeAppsData = [NSData dataWithContentsOfFile:appSchemesDictionaryPath
+                                                            options:NSDataReadingMappedIfSafe
+                                                              error:&dataError];
+            if (dataError && failureBlock) {
+                dispatch_async(dispatch_get_main_queue(), ^{
+                    failureBlock(dataError);
+                });
+            } else {
+                NSError *jsonError = nil;
+                NSDictionary *schemeAppsDictionary = [NSJSONSerialization JSONObjectWithData:schemeAppsData
+                                                                                     options:NSJSONReadingMutableLeaves
+                                                                                       error:&jsonError];
+                if (jsonError && failureBlock) {
+                    dispatch_async(dispatch_get_main_queue(), ^{
+                        failureBlock(jsonError);
+                    });
+                } else if (successBlock) {
+                    dispatch_async(dispatch_get_main_queue(), ^{
+                        successBlock(schemeAppsDictionary);
+                    });
+                }
+            }
+        }
+    });
+}
+
+
+#pragma mark - Helper methods
+
+- (NSArray *)subarraysOfArray:(NSArray *)array withCount:(NSInteger)subarraySize {
+    NSInteger rangeLocation = 0;
+    NSInteger arrayCount = [array count];
+    NSInteger itemsRemaining = [array count];
+    NSMutableArray *arrayOfArrays = [[NSMutableArray alloc] init];
+    while(rangeLocation < arrayCount) {
+        NSInteger rangeLength = MIN(subarraySize, itemsRemaining);
+        NSRange range = NSMakeRange(rangeLocation, rangeLength);
+        NSArray *subarray = [array subarrayWithRange:range];
+        [arrayOfArrays addObject:subarray];
+        itemsRemaining -= rangeLength;
+        rangeLocation += rangeLength;
+    }
+    return arrayOfArrays;
+}
+
+
+#pragma mark - Property methods
+
+- (NSString *)country {
+    if (!_country) {
+        NSLocale *currentLocale = [NSLocale currentLocale];
+        _country = [currentLocale objectForKey:NSLocaleCountryCode];
+    }
+    return _country;
+}
+
+@end
